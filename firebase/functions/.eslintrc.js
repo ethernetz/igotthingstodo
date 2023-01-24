441fedc1a2176e224b523961ca66f1b91a1f827f
Ethan Netz <ethannetz@hotmail.com>
2023-01-24 15:03:44 -0500

    fix linting

--- firebase/functions/.eslintrc.js
+++ firebase/functions/.eslintrc.js
@@ -1,21 +1,21 @@
 module.exports = {
-  root: true,
-  env: {
-    es6: true,
-    node: true,
-  },
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: ["tsconfig.json", "tsconfig.dev.json"],
-    sourceType: "module",
-    tsconfigRootDir: __dirname,
-  },
-  ignorePatterns: [
-    "/lib/**/*", // Ignore built files.
-  ],
-  extends: ["prettier"],
-  plugins: ["prettier"],
-  rules: {
-    "prettier/prettier": ["error"],
-  },
+	root: true,
+	env: {
+		es6: true,
+		node: true,
+	},
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		project: ['tsconfig.json', 'tsconfig.dev.json'],
+		sourceType: 'module',
+		tsconfigRootDir: __dirname,
+	},
+	ignorePatterns: [
+		'/lib/**/*', // Ignore built files.
+	],
+	extends: ['prettier'],
+	plugins: ['prettier'],
+	rules: {
+		'prettier/prettier': ['error'],
+	},
 };
